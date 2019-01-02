e94dd4f9b8a5c77e2f5e938934e90e8d906467da
ctncadi <ctncadi@gmail.com>
2019-01-02 17:58:03 +0530

    .

---
+++ app/controllers/admin/custom_load_reports_controller.rb
@@ -0,0 +1,12 @@
+class Admin::CustomLoadReportsController < ApplicationController
+	layout "admin/application"
+
+	def index
+		
+	end
+
+	def show
+		
+	end
+	
+end
---
+++ app/helpers/admin/custom_load_reports_helper.rb
@@ -0,0 +1,2 @@
+module Admin::CustomLoadReportsHelper
+end
