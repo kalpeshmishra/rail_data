995717002077eaf9de20a793eb8c95c20408f11a
ctncadi <ctncadi@gmail.com>
2019-01-07 17:55:41 +0530

    .

--- app/controllers/admin/custom_load_reports_controller.rb
+++ app/controllers/admin/custom_load_reports_controller.rb
@@ -35,7 +35,6 @@ class Admin::CustomLoadReportsController < ApplicationController
 
 		if params[:is_month_filter].present? and params[:selected_months].present?
 			months = params[:selected_months].split(",")
-			# no_of_month = months.length
 			data_hash = {}
 			months.each do |month|
 			 	month_name = month.upcase
@@ -77,8 +76,8 @@ class Admin::CustomLoadReportsController < ApplicationController
 		 	end	
 			@custom_month_station_commodity = commodity_list.compact.uniq
 		end
-		# i= 0
-		if params[:is_month_data_filter].present?
+		
+		if params[:is_month_data_filter].present? 
 			load_unload_ids = params[:selected_stations].split(',').map{|x|x.to_i}.delete_if {|x| x ==0}
 			major_commodity_ids = params[:selected_commodity].split(',').map{|x|x.to_i}.delete_if {|x| x ==0}
 			months = params[:selected_months].split(",")
@@ -92,21 +91,27 @@ class Admin::CustomLoadReportsController < ApplicationController
 				
 				rake_load_data.each do |data|
 					load_unload_code = LoadUnload.find(data.load_unload_id).station.code
-					major_commodity_code = MajorCommodity.find(data.major_commodity_id).major_commodity
+					if params[:selected_report_type] == "Commodity"
+						month_report_type = MajorCommodity.find(data.major_commodity_id).major_commodity
+					elsif params[:selected_report_type] == "Destination"
+						month_report_type = Station.find(data.station_id).code
+					else params[:selected_report_type] == "Stock"
+						month_report_type = WagonType.find(data.wagon_type_id).wagon_type_code
+					end
 					if data_hash[load_unload_code].present?
-						data_hash[load_unload_code].merge!("#{major_commodity_code}" => {}) if data_hash[load_unload_code][major_commodity_code].blank?
-						if data_hash[load_unload_code][major_commodity_code].keys.include?(month)
-							data_hash[load_unload_code][major_commodity_code][month] << data
+						data_hash[load_unload_code].merge!("#{month_report_type}" => {}) if data_hash[load_unload_code][month_report_type].blank?
+						if data_hash[load_unload_code][month_report_type].keys.include?(month)
+							data_hash[load_unload_code][month_report_type][month] << data
 						else
-							data_hash[load_unload_code][major_commodity_code].merge!("#{month}" => [data])
+							data_hash[load_unload_code][month_report_type].merge!("#{month}" => [data])
 						end
 					else	
 						data_hash[load_unload_code] ={}
-						data_hash[load_unload_code].merge!("#{major_commodity_code}" => {})
-						if data_hash[load_unload_code][major_commodity_code].keys.include?(month)
-							data_hash[load_unload_code][major_commodity_code][month] << data
+						data_hash[load_unload_code].merge!("#{month_report_type}" => {})
+						if data_hash[load_unload_code][month_report_type].keys.include?(month)
+							data_hash[load_unload_code][month_report_type][month] << data
 						else
-							data_hash[load_unload_code][major_commodity_code].merge!("#{month}" => [data])
+							data_hash[load_unload_code][month_report_type].merge!("#{month}" => [data])
 						end
 					end
 				end
@@ -114,8 +119,96 @@ class Admin::CustomLoadReportsController < ApplicationController
 			end
 			@custom_month_report_header = months
 			@custom_month_report_data = data_hash
-					# binding.pry if i<500
-					# binding.pry 
+		end
+
+		if params[:is_year_filter].present? and params[:selected_year].present?
+			year = params[:selected_year].split(",")
+			data_hash = {}
+			year.each do |year|
+			 	from_year = "Jan-"+year
+			 	to_year = "Dec-"+year
+			 	from_date = from_year.to_date.beginning_of_month 
+			 	to_date =	to_year.to_date.end_of_month
+			 	rake_load_data = RakeLoad.where(release_date: from_date..to_date)
+				data_hash.merge!("#{year}" => [rake_load_data])
+			end
+			
+			station_list = []
+			data_hash.values.each do |data|
+			 	data.flatten.each do |value|	
+			 		load_unload_code = LoadUnload.find(value.load_unload_id).station.code
+			 		station_list << [load_unload_code,value.load_unload_id ]
+			 	end
+		 	end	
+			@custom_load_report_year_station = station_list.compact.uniq
+		end
+
+		if params[:is_year_station_filter].present? and params[:selected_stations].present?
+			load_unload_ids = params[:selected_stations].split(',').map{|x|x.to_i}.delete_if {|x| x ==0}
+			year = params[:selected_year].split(",")
+			data_hash = {}
+			year.each do |year|
+			 	from_year = "Jan-"+year
+			 	to_year = "Dec-"+year
+			 	from_date = from_year.to_date.beginning_of_month 
+			 	to_date =	to_year.to_date.end_of_month
+			 	rake_load_data = RakeLoad.where(release_date: from_date..to_date, load_unload_id: load_unload_ids)
+				data_hash.merge!("#{year}" => [rake_load_data])
+			end
+			
+			commodity_list = []
+			data_hash.values.each do |data|
+			 	data.flatten.each do |value|	
+			 		commodity_code = MajorCommodity.find(value.major_commodity.id).major_commodity
+			 		commodity_list << [commodity_code,value.major_commodity.id ]
+			 	end
+		 	end	
+			@custom_year_station_commodity = commodity_list.compact.uniq
+		end
+
+		if params[:is_year_data_filter].present?
+			load_unload_ids = params[:selected_stations].split(',').map{|x|x.to_i}.delete_if {|x| x ==0}
+			major_commodity_ids = params[:selected_commodity].split(',').map{|x|x.to_i}.delete_if {|x| x ==0}
+			year = params[:selected_year].split(",")
+			
+			data_hash = {}
+			year.each do |year|
+			 from_year = "Jan-"+year
+			 	to_year = "Dec-"+year
+			 	from_date = from_year.to_date.beginning_of_month 
+			 	to_date =	to_year.to_date.end_of_month
+			 	rake_load_data = RakeLoad.where(release_date: from_date..to_date, load_unload_id: load_unload_ids, major_commodity_id: major_commodity_ids)
+				
+				rake_load_data.each do |data|
+					load_unload_code = LoadUnload.find(data.load_unload_id).station.code
+					if params[:selected_report_type] == "Commodity"
+						year_report_type = MajorCommodity.find(data.major_commodity_id).major_commodity
+					elsif params[:selected_report_type] == "Destination"
+						year_report_type = Station.find(data.station_id).code
+					else params[:selected_report_type] == "Stock"
+						year_report_type = WagonType.find(data.wagon_type_id).wagon_type_code
+					end
+					if data_hash[load_unload_code].present?
+						data_hash[load_unload_code].merge!("#{year_report_type}" => {}) if data_hash[load_unload_code][year_report_type].blank?
+						if data_hash[load_unload_code][year_report_type].keys.include?(year)
+							data_hash[load_unload_code][year_report_type][year] << data
+						else
+							data_hash[load_unload_code][year_report_type].merge!("#{year}" => [data])
+						end
+					else	
+						data_hash[load_unload_code] ={}
+						data_hash[load_unload_code].merge!("#{year_report_type}" => {})
+						if data_hash[load_unload_code][year_report_type].keys.include?(year)
+							data_hash[load_unload_code][year_report_type][year] << data
+						else
+							data_hash[load_unload_code][year_report_type].merge!("#{year}" => [data])
+						end
+					end
+				end
+				
+			end
+			@custom_year_report_header = year
+			@custom_year_report_data = data_hash
 		end
 
 	end
