116cf57d0eb1e60a2da7a2e13c6ec9b841b80c73
ctncadi <ctncadi@gmail.com>
2019-01-18 17:48:23 +0530

    .

--- app/controllers/admin/one_custom_reports_controller.rb
+++ app/controllers/admin/one_custom_reports_controller.rb
@@ -2,6 +2,7 @@ class Admin::OneCustomReportsController < ApplicationController
 	layout "admin/application"
 
 	def index
+		i =0
 		from_date = params[:from_date].to_date if params[:from_date].present?
 		to_date = params[:to_date].to_date if params[:to_date].present?
 		
@@ -16,73 +17,100 @@ class Admin::OneCustomReportsController < ApplicationController
 			major_commodity_id = load.major_commodity_id
 			major_commodity = MajorCommodity.find(major_commodity_id) rescue nil
 			rake_commodities = major_commodity.rake_commodities if major_commodity.present?
-			station_code = load.load_unload.station.code
-			# if station_code.present? and station_data_hash[station_code].present?
-				#Code for Commodity and LoadedUnit Total Starts
-				if load.create_rake_loads_rake_commodities.count == 0
-					loaded_unit = load.loaded_unit
-					commodity = major_commodity.major_commodity
-					if commodity.present? and data_hash[commodity].present?
-						final = data_hash[commodity].to_i + loaded_unit
-						data_hash[commodity] = final
+			station = load.load_unload.station.code
+			if load.create_rake_loads_rake_commodities.count == 0
+				loaded_unit = load.loaded_unit
+				commodity = major_commodity.major_commodity
+				if station.present? and data_hash[station].present?
+					if commodity.present? and data_hash[station][commodity].present?
+						final = data_hash[station][commodity].to_i + loaded_unit
+						data_hash[station][commodity] = final
 					else
-						data_hash.merge!("#{commodity}" => loaded_unit) if commodity.present?
-					end
-				elsif major_commodity.major_commodity == "GG" and load.create_rake_loads_rake_commodities.count > 0
-					load.create_rake_loads_rake_commodities.each do |gg_load|
-						loaded_unit = gg_load.rake_unit
-						commodity = RakeCommodity.find(gg_load.rake_commodity_id).rake_commodity_code
-						if commodity.present? and data_hash[commodity].present?
-							final = data_hash[commodity].to_i + loaded_unit
-							data_hash[commodity] = final
-						else
-							data_hash.merge!("#{commodity}" => loaded_unit) if commodity.present?
-						end
-					end
-				elsif major_commodity.major_commodity == "FG" and load.create_rake_loads_rake_commodities.count > 0
-					load.create_rake_loads_rake_commodities.each do |fg_load|
-						loaded_unit = fg_load.rake_unit
-						commodity = RakeCommodity.find(fg_load.rake_commodity_id).rake_commodity_code
-						if commodity.present? and data_hash[commodity].present?
-							final = data_hash[commodity].to_i + loaded_unit
-							data_hash[commodity] = final
-						else
-							data_hash.merge!("#{commodity}" => loaded_unit) if commodity.present?
-						end
+						data_hash[station].merge!("#{commodity}" => loaded_unit) if commodity.present?
 					end
 				else
-					load.create_rake_loads_rake_commodities.each do |other_load|
-					commodity = RakeCommodity.find(other_load.rake_commodity_id).rake_commodity_code
-						if MajorCommodity.find_by(major_commodity: commodity).present?
-							loaded_unit = other_load.rake_unit
-							if commodity.present? and data_hash[commodity].present?
-								final = data_hash[commodity].to_i + loaded_unit
-								data_hash[commodity] = final
-							else
-								data_hash.merge!("#{commodity}" => loaded_unit) if commodity.present?
-							end
+						data_hash[station] = {}
+						data_hash[station].merge!("#{commodity}" => loaded_unit) 
+				end
+				
+			elsif major_commodity.major_commodity == "GG" and load.create_rake_loads_rake_commodities.count > 0
+				load.create_rake_loads_rake_commodities.each do |gg_load|
+					loaded_unit = gg_load.rake_unit
+					commodity = RakeCommodity.find(gg_load.rake_commodity_id).rake_commodity_code
+					if station.present? and data_hash[station].present?
+						if commodity.present? and data_hash[station][commodity].present?
+							final = data_hash[station][commodity].to_i + loaded_unit
+							data_hash[station][commodity] = final
 						else
-							loaded_unit = other_load.rake_unit
-							major_id = RakeCommodity.find(other_load.rake_commodity_id).major_commodity_id
-							other_commodity = MajorCommodity.find(major_id).major_commodity
-							if other_commodity.present? and data_hash[other_commodity].present?
-								final = data_hash[other_commodity].to_i + loaded_unit
-								data_hash[other_commodity] = final
-							else
-								data_hash.merge!("#{other_commodity}" => loaded_unit) if other_commodity.present?
-							end
+							data_hash[station].merge!("#{commodity}" => loaded_unit) if commodity.present?
 						end
+					else
+							data_hash[station] = {} if data_hash[station].blank?
+							data_hash[station].merge!("#{commodity}" => loaded_unit) 
 					end
 				end
-				#Code for Commodity and LoadedUnit Total Ends
-			# end
 
+			elsif major_commodity.major_commodity == "FG" and load.create_rake_loads_rake_commodities.count > 0
+				load.create_rake_loads_rake_commodities.each do |fg_load|
+					loaded_unit = fg_load.rake_unit
+					commodity = RakeCommodity.find(fg_load.rake_commodity_id).rake_commodity_code
+					if station.present? and data_hash[station].present?
+						if commodity.present? and data_hash[station][commodity].present?
+							final = data_hash[station][commodity].to_i + loaded_unit
+							data_hash[station][commodity] = final
+						else
+							data_hash[station].merge!("#{commodity}" => loaded_unit) if commodity.present?
+						end
+					else
+							data_hash[station] = {} if data_hash[station].blank?
+							data_hash[station].merge!("#{commodity}" => loaded_unit) 
+					end
+				end
+			else
+				load.create_rake_loads_rake_commodities.each do |other_load|
+				commodity = RakeCommodity.find(other_load.rake_commodity_id).rake_commodity_code
+					if MajorCommodity.find_by(major_commodity: commodity).present?
+						loaded_unit = other_load.rake_unit
+						if station.present? and data_hash[station].present?
+							if commodity.present? and data_hash[station][commodity].present?
+								final = data_hash[station][commodity].to_i + loaded_unit
+								data_hash[station][commodity] = final
+							else
+								data_hash[station].merge!("#{commodity}" => loaded_unit) if commodity.present?
+							end
+						else
+							data_hash[station] = {}
+							data_hash[station].merge!("#{commodity}" => loaded_unit) 
+						end
+					else
+						loaded_unit = other_load.rake_unit
+						major_id = RakeCommodity.find(other_load.rake_commodity_id).major_commodity_id
+						other_commodity = MajorCommodity.find(major_id).major_commodity
+						if other_commodity.present? and data_hash[station][other_commodity].present?
+							final = data_hash[station][other_commodity].to_i + loaded_unit
+							data_hash[station][other_commodity] = final
+						else
+							data_hash[station] = {} if data_hash[station].blank?
+							data_hash[station].merge!("#{other_commodity}" => loaded_unit) if other_commodity.present?
+						end
+
+					end
+
+				end
+			end
+			
 		end
-		@one_custom_commodity_list = data_hash.keys.map{|x| [x,x]}
-		
-
+		@one_custom_commodity_list = []
+		data_hash.each do |station,data|
+			@one_custom_commodity_list <<data.keys[0]
+		end
+		@one_custom_commodity_list = @one_custom_commodity_list.uniq
+		@one_custom_commodity_list = @one_custom_commodity_list.map{|x| [x,x]}
 		if params[:is_data_filter].present?
+		# binding.pry if i <700
+		
 			selected_commodity_codes = params[:selected_commodity].split(',').map{|x|x}.delete_if {|x| x =="multiselect-all"}
+			# binding.pry
 			
 			
 		end
