b5794526b20f67c2ce64938ef4f58fe9c5161bb5
kalpeshmishra <kalpeshmishra8@gmail.com>
2019-02-02 19:11:37 +0530

    .

--- app/controllers/admin/unloading_reports_controller.rb
+++ app/controllers/admin/unloading_reports_controller.rb
@@ -32,9 +32,51 @@ class Admin::UnloadingReportsController < ApplicationController
     @total_adi_unloads = adi_unit
 		@total_gimb_unloads = gimb_unit
     @total_rake_unloads = adi_unit + gimb_unit 
+
+    get_abc_summary_data(data)
     
   end
-  
+  def get_abc_summary_data(data)
+    # previous_date = data-1
+    previous_stock = RakeUnload.where(RakeUnload.arel_table[:placement_date].lt(data).and(RakeUnload.arel_table[:release_date].gteq(data)))
+    previous_stock += RakeUnload.where(RakeUnload.arel_table[:placement_date].lt(data).and(RakeUnload.arel_table[:release_date].eq(nil)))
+    previous_stock += RakeUnload.where(RakeUnload.arel_table[:placement_date].lt(data).and(RakeUnload.arel_table[:release_date].eq("")))
+    
+    received_stock = RakeUnload.where(RakeUnload.arel_table[:placement_date].eq(data))
+    release_stock = RakeUnload.where(RakeUnload.arel_table[:release_date].eq(data))
+
+    onhand_stock = RakeUnload.where(RakeUnload.arel_table[:placement_date].lteq(data).and(RakeUnload.arel_table[:release_date].gt(data)))
+    onhand_stock += RakeUnload.where(RakeUnload.arel_table[:placement_date].lteq(data).and(RakeUnload.arel_table[:release_date].eq(nil)))
+    onhand_stock += RakeUnload.where(RakeUnload.arel_table[:placement_date].lteq(data).and(RakeUnload.arel_table[:release_date].eq("")))
+    
+    all_stock = previous_stock + received_stock + release_stock + onhand_stock
+    adi_stations = []
+    gimb_stations = []
+    all_stock.each do |rake_unload|
+      rake_area =  rake_unload.load_unload.station.area.area_code 
+      if rake_area == "ADI"
+        adi_stations << rake_unload.load_unload.station.code
+      elsif rake_area == "GIMB"
+        gimb_stations << rake_unload.load_unload.station.code
+      end
+    end
+    adi_stations = adi_stations.uniq  
+    gimb_stations = gimb_stations.uniq
+
+    adi_data_hash = {}
+    if adi_stations.present?
+      adi_stations.each do|station|
+        adi_data_hash[station] ={}
+        previous_stock.each do |rake_unload|
+          adi_data_hash[station].merge!("previous_stock" => {})
+          stock_type = WagonType.find(rake_unload.wagon_type_id).wagon_details_covered_open
+          loaded_unit = rake_unload.loaded_unit
+          adi_data_hash[station]["previous_stock"].merge!(stock_type => loaded_unit)
+        end
+      end
+        binding.pry
+    end
+  end
 
   def show
     
