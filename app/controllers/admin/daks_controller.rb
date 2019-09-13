bc309a47108c323d54228c4dedb4c716a668683a
ctncadi <ctncadi@gmail.com>
2019-09-13 17:54:59 +0530

    View-hide details toggle added in dak dispatch and search

--- app/controllers/admin/daks_controller.rb
+++ app/controllers/admin/daks_controller.rb
@@ -121,12 +121,12 @@ class Admin::DaksController < ApplicationController
 		user_list = User.find(UserRole.where(is_dak_access: true).pluck(:user_id))
 
 		dak_user_list = {}
-		dak_user_list.merge!("AdiDivision" => {})
+		dak_user_list.merge!("ADI-Division" => {})
 		dak_search_user = []
 		user_list.each do |user|
 			if user.id != current_user.id
-				dak_user_list["AdiDivision"].merge!("#{user.user_under}" => {}) if dak_user_list["AdiDivision"].keys.exclude?(user.user_under)
-				dak_user_list["AdiDivision"][user.user_under].merge!("#{user.first_name}" => user.id)	
+				dak_user_list["ADI-Division"].merge!("#{user.user_under}" => {}) if dak_user_list["ADI-Division"].keys.exclude?(user.user_under)
+				dak_user_list["ADI-Division"][user.user_under].merge!("#{user.first_name}" => user.id)	
 				dak_search_user << [user.first_name,user.id]
 			end
 		end
