34e73a5fc2371af2cc6aea51f319c208e1465439
kalp <ctncadi@gmail.com>
2020-02-03 17:48:38 +0530

    EMployee allowance views modified

--- app/controllers/admin/employee_allowances_controller.rb
+++ app/controllers/admin/employee_allowances_controller.rb
@@ -7,6 +7,26 @@ class Admin::EmployeeAllowancesController < ApplicationController
 
   def new
   	get_form_data
+  	if params[:is_station_filter].present?
+  		stn_id = params[:selected_station_id].to_i
+  		@employee_list = Employee.where(station_id: stn_id).map{ |emp| [[emp.emp_number,emp.first_name,emp.last_name].join("-"),emp.id]}
+  	end
+
+  	if params[:is_data_filter].present?
+  		selected_employee_id = params[:selected_employee].split(',').map!{|e| e.to_i}.delete_if {|x| x ==0}
+  		station = params["station"].to_i
+	  	selected_month = params["selected_month"]
+	  	employee_allowance_data = {}
+	  	selected_employee_id.map{|no|
+	  		emp = Employee.where(id: no).pluck(:id,:first_name,:last_name,:emp_number)
+	  		employee_allowance_data[emp] = {}
+	  	}
+	  	data = EmployeeAllowance.where(month: selected_month, employee_id: selected_employee_id, station_id: station) 
+	  	data.map{ |e| employee_allowance_data[e.employee_id] = e }
+  		
+  		@employee_allowance_selected_data = employee_allowance_data
+  	end
+
   end
 
   def create
